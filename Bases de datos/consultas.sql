68dca024a8647e7cf53d95d61ba92b1156760d00
unknown <dcorderoflorez@correo.unicordoba.edu.co>
2018-02-08 14:39:45 -0500

    copia4

---
+++ Bases de datos/consultas.sql
@@ -0,0 +1,10 @@
+	DELETE * FROM Libros WHERE nombre= “Harry Potter”
+
+	UPDATE Usuario SET apellido2= “Rodríguez” WHERE nombre1= “Antonio” AND apellido1= “García” AND apellido2= “Benites”
+	
+        INSERT INTO Usuario (nombre1, apellido1) VALUES (“Miguel”, “López”)
+
+	SELECT nombre, descripción FROM Libros WHERE fecha_publicacion= “02/02/2017”
+
+	UPDATE libros SET nombre="el amor" WHERE nombre=" sentimientos"
+
